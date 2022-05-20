e430d03ecd0fb06a440f3c21d46d86b668051e12
Sayak Kundu <sayakk1998@gmail.com>
2022-05-20 20:05:32 +0530

    ADDED EMAIL VERIFICATION

--- try2/settings.py
+++ try2/settings.py
@@ -133,3 +133,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_HOST_USER = 'sayakjontykundu@gmail.com'
+EMAIL_HOST_PASSWORD = 'morqyzakyncbtfuh'
+EMAIL_USE_TLS = True 
--- try2/urls.py
+++ try2/urls.py
@@ -13,12 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vac.urls')),
     path('accounts/', include('accounts.urls')),
-    path('slotbook/', include('slotbook.urls'))
+    path('slotbook/', include('slotbook.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name = 'password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name = 'password_reset_done'),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name = 'password_reset_confirm'),
+    path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name = 'password_reset_complete')
 ]
